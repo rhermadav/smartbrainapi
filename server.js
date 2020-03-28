266c4198f7e59582cf1d6ab4949f2ab176f2f2c7
rhermadav <davisonuyigue@gmail.com>
2020-03-28 03:02:15 +0100

    instaling latest package

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt-nodejs');
+const bcrypt = require('bcrypt');
 const cors = require('cors');
 const knex = require('knex');
 
@@ -31,6 +31,7 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
-app.listen(process.env.PORT || 3000, ()=> {
-  console.log(`app is running on port ${process.env.PORT}`);
+const port =process.env.PORT || 3000;
+app.listen(port, ()=> {
+  console.log(`app is running on port ${port}`);
 })
