0e3fd845d72a5cb20e4f7aa916509648cb10a6e9
rhermadav <davisonuyigue@gmail.com>
2020-03-31 23:24:04 +0100

    hash password

--- controllers/register.js
+++ controllers/register.js
@@ -1,9 +1,10 @@
-const handleRegister = (req, res, db, bcrypt) => {
+const handleRegister = async (req, res, db, bcrypt) => {
   const { email, name, password } = req.body;
   if (!email || !name || !password) {
     return res.status(400).json('incorrect form submission');
   }
-  const hash = bcrypt.hashSync(password);
+  const salt = await bcrypt.genSalt(10);
+  const hash =await bcrypt.hash(password ,salt);
     db.transaction(trx => {
       trx.insert({
         hash: hash,
